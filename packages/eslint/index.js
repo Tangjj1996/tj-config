b9462d442c8e6000781344c1c98fdeff29d2feea
Tangjj1996 <awesometangji@gmail.com>
2022-09-10 09:08:50 +0800

    feat: base eslint update

--- packages/eslint/index.js
+++ packages/eslint/index.js
@@ -51,11 +51,11 @@ module.exports = {
     "import/no-duplicates": ["error", { considerQueryString: true }],
 
     // Common
-    semi: ["error", "never"],
+    semi: ["error", "always"],
     curly: ["error", "multi-or-nest", "consistent"],
     "linebreak-style": "off",
-    quotes: ["error", "single"],
-    "quote-props": ["error", "consistent-as-needed"],
+    quotes: ["error", "double"],
+    "quote-props": ["error", "as-needed"],
     "no-bitwise": ["error", { allow: ["~", ">>", "<<", "&", "|"] }],
     "no-unused-vars": "warn",
     "no-param-reassign": [
